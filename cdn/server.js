172a0f6543ecd5fe4349fbfbc7f4fe7bd0295e14
joaoamaral28 <joaop.amaral@ua.pt>
2018-10-14 02:02:37 +0100

    Node server operational. New page created for each different movie request Added closeStream api call (might be useless, time will tell)

--- cdn/server.js
+++ cdn/server.js
@@ -2,23 +2,47 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var opn = require('opn');
 
 app.get('/streamMovie/:id', function (req, res) {
+
+	// CORS headers
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 	if(fs.existsSync( __dirname + "/movies/" + req.params.id + ".mp4")){
 		console.log("Movie found");
 		res.statusCode = 200;
-		return res.end();
+		renderWebpage(req.params.id);
+		return res.json({ notice : ['Movie found!'] });
 	}else
 		console.log("Movie not found");
 		res.statusCode = 404;
       	return res.json({ errors: ['Movie not found!'] });
 
-})
+});
+
+app.post('closeStream/:id', function (req, res){
+
+	// CORS headers
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+	// TODO
+	// << logic to stop stream and following operations goes here >>
+
+	/*
+	fs.unlink("new_server"+movie_id+".html", function(err){
+		if(err) throw err;
+		console.log("Page removed");
+	});
+	*/
+
+});
 
 app.all('*', function(req, res) {
     throw new Error("Invalid request")
-})
+});
 
 var server = app.listen(1935, function () {
 
@@ -26,4 +50,49 @@ var server = app.listen(1935, function () {
 	var port = server.address().port
 
 	console.log("Server listening at http://%s:%s", host, port);
-})
+
+});
+
+
+function renderWebpage(movie_id){
+
+	// delete old html page if existent
+	
+	fs.unlink("./new_server"+movie_id+".html", function(err){
+		if(err) throw err;
+		console.log("Old page removed");
+	});
+	
+	fs.appendFile("new_server"+movie_id+".html",
+		"<!doctype html> \
+		<html lang=\"en\"> \
+		<head> \
+		<meta charset=\"utf-8\"> \
+		<title>Server side</title> \
+		<link rel=\"stylesheet\" href=\"css/styles.css?v=1.0\"> \
+		<style> \
+		    video { \
+		        width: 800px; \
+		        height:600px; \
+		    } \
+		</style> \
+		</head> \
+		<body onload=\"pageReady()\"> \
+		<div align=\"center\" border-style=\"hidden\" border-width=\"5 px \"> \
+			Movie " + movie_id + " title \
+		</div> \
+		<div id=\"player_div\" align=\"center\"> \
+        	<video controls id=\"sourceVideoPlayer\" src=\"./movies/"+movie_id+".mp4\" type=\"video/mp4\">Video not available.</video> \
+    	</div> \
+		  <script src=\"server_stream.js\"></script> \
+		</body> \
+		</html>" ,
+
+	function (err){
+		if(err) throw err;
+		console.log("Page created successfuly!");
+	});
+
+
+	opn("./new_server"+movie_id+".html");
+}
---
+++ cdn/server_stream.js
@@ -0,0 +1,20 @@
+
+
+function pageReady(){
+
+	console.log("pageReady()");
+
+	// TODO 
+	// << streaming logic goes here >>
+
+	// localConnection = new RTCPeerConnection();
+
+	//var movie_id = window.location.href.split("/");
+
+	//var video = document.createElement('video');
+
+	//video.src = "./movies/"+movie_id[movie_id.length-1];
+
+	//video.play();
+
+}
