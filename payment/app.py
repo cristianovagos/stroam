1fab24f973ac2715520ddfaa0ba26fd27e54bd2f
asergio <asergio@ua.pt>
2018-12-02 00:01:29 +0000

    Config changes for Payment host on /pay

--- payment/app.py
+++ payment/app.py
@@ -11,7 +11,7 @@ from apispec.ext.flask import FlaskPlugin
 import database as db
 import secrets, json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = '/pay')
 app.config.from_pyfile('config.py')
 
 # Create an APISpec
@@ -1208,5 +1208,24 @@ with open('static/swagger.json', 'w') as f:
     json.dump(spec.to_dict(), f)
 ###
 
+# Solution to add prefix - 
+# Credits: https://stackoverflow.com/a/36033627/9883550
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["Page not found.".encode()]
+
 if __name__ == '__main__':
+    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/pay')
     app.run(debug=True, host='0.0.0.0', port=5000) #run app in debug mode on port 5000
